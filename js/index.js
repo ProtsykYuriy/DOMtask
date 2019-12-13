283a1a463cabe157c91ec9a683fc2b51fa50c2d4
ProtsykYuriy <protsyk.yura85@gmail.com>
2019-12-13 12:22:39 +0000

    Comment

--- js/index.js
+++ js/index.js
@@ -1,47 +1,29 @@
-/*let ulShopingList = document.querySelector('ul');
-let inputShoppingList= document.querySelector('input');
-let buttonAddItem= document.querySelector('button');
-let input=document.querySelector('#item');
-let buttonClick = function(){
-    inputInfo=input.value;
-
-}
-buttonAddItem.addEventListener(click, buttonClick);
-let li = createElement('li');
-li.textContent='Eggs'
-let span = createElement('span');
-let buttonDelete = createElement('button');
-ulShopingList.appendChild('span');
-ulShopingList.appendChild('button');
-span.textContent = inputValue;
-buttonDelete.textContent = 'delete';
-ulShopingList.appendChild('li');
-buttonDelete.classList.add('delete')
-buttonDelete.addEventListener(click,inputDelete)
-let inputDelete = function(){
-    input.value = '';
-}*/
 let ul = document.querySelector('ul');
 let input = document.querySelector('#item');
 let buttonAddItem = document.querySelector('#addItem');
-
 let inputValue;
-let getValue = function(){
-    inputValue = input.value;
-    console.log(inputValue);
+let getValue = function () {
+  inputValue = input.value;
+  let newLi = li.cloneNode(false);
+  let newButtonDelete = buttonDelete.cloneNode(true);
+  newButtonDelete.addEventListener('click', deleteItem);
+  let newSpan = span.cloneNode(true);
+  newSpan.textContent = inputValue;
+  newLi.appendChild(newSpan);
+  newLi.appendChild(newButtonDelete);
+  ul.appendChild(newLi);
+  input.value = '';
+  input.focus();
 };
 buttonAddItem.addEventListener('click', getValue);
 let li = document.createElement('li');
 let span = document.createElement('span');
-let buttonDelete=document.createElement('button');
-ul.appendChild(span, buttonDelete);
-li.textContent = inputValue;
-buttonDelete.textContent='delete';
-ul.appendChild(li);
-let deleteList = function(){
-    ul.childNodes.remove();
-}
-buttonDelete.addEventListener('click',deleteList)
-
-
-
+let buttonDelete = document.createElement('button');
+li.appendChild(span);
+li.appendChild(buttonDelete);
+span.textContent = inputValue;
+buttonDelete.textContent = 'delete';
+let deleteItem = function () {
+  document.body.children.remove();
+  input.focus();
+};
